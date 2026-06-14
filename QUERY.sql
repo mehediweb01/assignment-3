f4d9f0a02fcb2863ac34f238997bf895a9579ab7
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2026-06-14 19:53:16 +0600

    query 6 done

--- QUERY.sql
+++ QUERY.sql
@@ -53,3 +53,18 @@ select
 from
   bookings as b
   full join users as u on u.user_id = b.user_id;
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+select
+  booking_id,
+  match_id,
+  round(total_cost)
+from
+  bookings
+where
+  total_cost > (
+    select
+      avg(total_cost)
+    from
+      bookings
+  );
