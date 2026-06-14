69c532624279391229b64232d70c2a99e4e5c447
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2026-06-14 17:36:27 +0600

    query 1 done

--- QUERY.sql
+++ QUERY.sql
@@ -1,1 +1,12 @@
 -- assignment 3 - programming hero
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+select
+  match_id,
+  fixture,
+  round(base_ticket_price)
+from
+  matches
+where
+  tournament_category = 'Champions League'
+  and match_status = 'Available';
